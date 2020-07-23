344f8589ad091d8ac87ade31c13d1fd73a6a59f3
BessAleks <bessaleks@yandex.ru>
2020-07-23 14:09:41 +0300

    add homework

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -11,9 +11,31 @@ public class CustomerStorage
 
     public void addCustomer(String data)
     {
-        String[] components = data.split("\\s+");
-        String name = components[0] + " " + components[1];
-        storage.put(name, new Customer(name, components[3], components[2]));
+        try {
+            String[] components = data.split("\\s+");
+            if(components.length != 4){
+                throw new Exception("Wrong length!");
+            }
+            else if(!components[0].matches("[а-яА-Я-]+")){
+                throw new Exception("Wrong name!");
+            }
+            else if(!components[1].matches("[а-яА-Я-]+")){
+                throw new Exception("Wrong surname!");
+            }
+            else if(!components[2].matches("^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$")){
+                throw new Exception("Wrong eMail!");
+            }
+            else if(!components[3].matches("^[0-9-]+$")){
+                throw new Exception("Wrong phone!");
+            }
+            else {
+                String name = components[0] + " " + components[1];
+                storage.put(name, new Customer(name, components[3], components[2]));
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
     }
 
     public void listCustomers()
--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
@@ -2,12 +2,9 @@ import java.util.Scanner;
 
 public class Main
 {
-    private static String addCommand = "add Василий Петров " +
-            "vasily.petrov@gmail.com +79215637722";
-    private static String commandExamples = "\t" + addCommand + "\n" +
-            "\tlist\n\tcount\n\tremove Василий Петров";
-    private static String commandError = "Wrong command! Available command examples: \n" +
-            commandExamples;
+    private static String addCommand = "add Василий Петров " + "vasily.petrov@gmail.com +79215637722";
+    private static String commandExamples = "\t" + addCommand + "\n" + "\tlist\n\tcount\n\tremove Василий Петров";
+    private static String commandError = "Wrong command! Available command examples: \n" + commandExamples;
     private static String helpText = "Command examples:\n" + commandExamples;
 
     public static void main(String[] args)
